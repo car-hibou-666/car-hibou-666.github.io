34b26e6e134092a15e590750caa86d1b61c8de75
car-hibou-666 <car-hibou-666@keemail.me>
2022-09-16 10:20:31 +0200

    Test flux RSS

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -47,7 +47,10 @@ const config = {
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
+            feedOptions: {
+              type: 'all',
+            },
+          },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
